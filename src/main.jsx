7aa946eb9951927ab5961d83130cec06248b4675
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-10-09 21:12:04 +0100

    basic styles added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+// import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+// import { store, persistor } from "./redux/store.js";
+import App from "./App.jsx";
+import "./index.css";
+import "modern-normalize/modern-normalize.css";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate> */}
+    {/* </Provider> */}
+  </StrictMode>
+);
