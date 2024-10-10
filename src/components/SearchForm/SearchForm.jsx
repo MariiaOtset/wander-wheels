87813b496a102db354c504e00f36f298b267e0fd
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-10-10 21:24:08 +0100

    location search added to SearchForm

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,40 @@
 import css from "./SearchForm.module.css";
 import { Formik, Form, Field } from "formik";
+import sprite from "../../images/sprite.svg";
+import { useState } from "react";
 
 const SearchForm = () => {
+  const [value, setValue] = useState("");
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
     <Formik>
       <Form>
-        {/* Location filter */}
-        <div>
-          <Field className={css.input} />
+        {/* Location */}
+        <label htmlFor="location" className={css.greyText}>
+          Location
+        </label>
+        <div className={css.locationWrapper}>
+          <Field
+            className={`${css.inputLocation} ${css.inputFocus}`}
+            onChange={handleChange}
+            name="location"
+            id="location"
+            list="location-name"
+            placeholder="City"
+          />
+          <svg
+            className={`${css.locationIcon} ${value ? "filled" : ""}`}
+            width="20"
+            height="20"
+          >
+            <use href={`${sprite}#icon-map`}></use>
+          </svg>
         </div>
+
+        {/* Filters */}
+        <div></div>
       </Form>
     </Formik>
   );
