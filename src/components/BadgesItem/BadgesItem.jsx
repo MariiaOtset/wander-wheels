4b71ada2a200962198c75078a8e4cdf4812d4524
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-10-12 13:35:27 +0100

    camper equipment added to camper card

--- src/components/BadgesItem/BadgesItem.jsx
+++ src/components/BadgesItem/BadgesItem.jsx
@@ -1,7 +1,40 @@
 import css from "./BadgesItem.module.css";
+import sprite from "../../images/sprite.svg";
 
-const BadgesItem = () => {
-  return <div className={css.itemWrapper}>BadgesItem</div>;
+// const BadgesItem = ({ badge }) => {
+//   return (
+//     <div className={css.itemWrapper}>
+//       {sprite.includes(badge.toLowerCase()) && (
+//         <svg className={css.badgeIcon} width="20" height="20">
+//           <use href={`${sprite}#icon-${badge.toLowerCase()}`}></use>
+//         </svg>
+//       )}
+//       <p>{badge}</p>
+//     </div>
+//   );
+// };
+
+const BadgesItem = ({ badge }) => {
+  const iconName = badge.toLowerCase();
+  const iconExists = `${sprite}#icon-${iconName}`;
+
+  return (
+    <div className={css.itemWrapper}>
+      {iconExists ? (
+        <div className={css.wrapp}>
+          <svg className={css.badgeIcon}>
+            <use href={`${sprite}#icon-${iconName}`}></use>
+          </svg>
+          <p>{badge}</p>
+        </div>
+      ) : (
+        <div className={css.withoutIcon}>
+          <svg className={css.hidden}></svg>
+          <p className={css.textOnly}>{badge}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default BadgesItem;
--- src/components/BadgesList/BadgesList.jsx
+++ src/components/BadgesList/BadgesList.jsx
@@ -1,16 +1,21 @@
+import BadgesItem from "../BadgesItem/BadgesItem.jsx";
 import css from "./BadgesList.module.css";
 
 const BadgesList = ({ camper }) => {
   return (
-    <ul className={css.wrapper}>
-      {
-        //     camper.map((item) => {
-        //         return(
-        //             <li>{i}</li>
-        //       )
-        //   })
-      }
-    </ul>
+    <div className={css.wrapper}>
+      {camper.transmission === "automatic" && <BadgesItem badge="Automatic" />}
+      {camper.engine === "petrol" && <BadgesItem badge="Petrol" />}
+      {camper.AC && <BadgesItem badge="AC" />}
+      {camper.bathroom && <BadgesItem badge="Bathroom" />}
+      {camper.kitchen && <BadgesItem badge="Kitchen" />}
+      {camper.TV && <BadgesItem badge="TV" />}
+      {camper.radio === "petrol" && <BadgesItem badge="Radio" />}
+      {camper.refrigerator && <BadgesItem badge="Refrigerator" />}
+      {camper.microwave && <BadgesItem badge="Microwave" />}
+      {camper.gas && <BadgesItem badge="Gas" />}
+      {camper.water && <BadgesItem badge="Water" />}
+    </div>
   );
 };
 
