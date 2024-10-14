b2f00329b0f7ecc442247bc1386c9d0742ed05cd
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-10-14 09:50:54 +0100

    camper reviews done

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,16 +1,29 @@
 import css from "./ReviewItem.module.css";
+import sprite from "../../images/sprite.svg";
 
 const ReviewItem = ({ review }) => {
   const { reviewer_name, reviewer_rating, comment } = review;
   return (
     <div>
       <div className={css.wrapper}>
-        <div className={css.avatar}> {reviewer_name[0]}</div>
+        <div className={css.avatar}>{reviewer_name[0]}</div>
         <div className={css.nameWithStars}>
           <p className={css.name}>{reviewer_name}</p>
-          <div>{}</div>
+          <div>
+            {[...Array(5)].map((_, index) => (
+              <svg
+                key={index}
+                className={index < reviewer_rating ? css.yellow : css.grey}
+                width="16"
+                height="16"
+              >
+                <use href={`${sprite}#icon-star`}></use>
+              </svg>
+            ))}
+          </div>
         </div>
       </div>
+      <p className={css.text}>{comment}</p>
     </div>
   );
 };
