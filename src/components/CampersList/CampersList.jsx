f88edda44966cb765433fddee9b194ced6b4a821
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-10-16 13:57:19 +0100

    fixed fonts connection

--- src/components/CampersList/CampersList.jsx
+++ src/components/CampersList/CampersList.jsx
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import { selectCampers } from "../../redux/campers/selectors.js";
 import CampersItem from "../CampersItem/CampersItem.jsx";
 import css from "./CampersList.module.css";
-// import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn.jsx";
 
 const CampersList = () => {
   const campers = useSelector(selectCampers);
@@ -18,7 +17,6 @@ const CampersList = () => {
           );
         })}
       </ul>
-      {/* <LoadMoreBtn /> */}
     </div>
   );
 };
